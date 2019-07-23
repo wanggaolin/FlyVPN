3e98e812b3258e17cf1b13f941f93bdd1b9e6a0d
SmallPig <664235822@qq.com>
2019-07-23 23:01:09 +0800

    制作登录界面

---
+++ app/src/main/java/com/smallpig/flyvpn/ui/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.smallpig.flyvpn.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import com.smallpig.flyvpn.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        loginButton = findViewById(R.id.button_login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "登录成功", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
--- app/src/main/java/com/smallpig/flyvpn/ui/MainActivity.java
+++ app/src/main/java/com/smallpig/flyvpn/ui/MainActivity.java
@@ -1,5 +1,6 @@
-package com.smallpig.flyvpn;
+package com.smallpig.flyvpn.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.smallpig.flyvpn.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,6 +43,11 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+
+        if (id == R.id.action_login) {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
