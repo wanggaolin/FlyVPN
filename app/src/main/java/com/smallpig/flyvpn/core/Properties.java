4cdcdae8155691f0cf2e0cd4c2a6c2740a9a0fd1
SmallPig <664235822@qq.com>
2019-08-03 19:05:00 +0800

    更改json地址

--- app/src/main/java/com/smallpig/flyvpn/core/Properties.java
+++ app/src/main/java/com/smallpig/flyvpn/core/Properties.java
@@ -1,7 +1,7 @@
 package com.smallpig.flyvpn.core;
 
 public class Properties {
-    public static String jsonPath = "https://raw.githubusercontent.com/664235822/Test/master/content.json";
+    public static String jsonPath = "https://raw.githubusercontent.com/664235822/FlyVPN/master/content.json";
     public static String proxyURL = "ss://aes-256-cfb:156318aq@119.28.8.50:443";
     public static String mysqlURL = "jdbc:mysql://119.28.8.50/flyvpn?";
 
